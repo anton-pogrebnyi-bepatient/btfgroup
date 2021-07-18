40ea64c5765ccff59d3a0ec3aaaf49ae5d125bde
Anton Pogrebnyi <anton.pogrebnyi@bepatient.com>
2021-07-19 09:53:53 +1000

    Module fix

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,127 @@
+import { Injectable } from '@angular/core';
+
+export interface Message {
+  fromName: string;
+  subject: string;
+  date: string;
+  id: number;
+  read: boolean;
+}
+
+export interface Comment {
+  body: string;
+  email: string;
+  name: string;
+  id: number;
+  postId: number;
+}
+
+export interface Post {
+  body: string;
+  title: string;
+  id: number;
+  userId: number;
+}
+
+export interface User {
+  address: Address;
+  company: Company;
+  email: string;
+  name: string;
+  phone: string;
+  id: number;
+  username: boolean;
+  website: string;
+}
+
+export interface Company {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+}
+export interface Address {
+  city: string;
+  geo: Geo;
+  street: string;
+  suite: string;
+  zipcode: string;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  public messages: Message[] = [
+    {
+      fromName: 'Matt Chorsey',
+      subject: 'New event: Trip to Vegas',
+      date: '9:32 AM',
+      id: 0,
+      read: false
+    },
+    {
+      fromName: 'Lauren Ruthford',
+      subject: 'Long time no chat',
+      date: '6:12 AM',
+      id: 1,
+      read: false
+    },
+    {
+      fromName: 'Jordan Firth',
+      subject: 'Report Results',
+      date: '4:55 AM',
+      id: 2,
+      read: false
+    },
+    {
+      fromName: 'Bill Thomas',
+      subject: 'The situation',
+      date: 'Yesterday',
+      id: 3,
+      read: false
+    },
+    {
+      fromName: 'Joanne Pollan',
+      subject: 'Updated invitation: Swim lessons',
+      date: 'Yesterday',
+      id: 4,
+      read: false
+    },
+    {
+      fromName: 'Andrea Cornerston',
+      subject: 'Last minute ask',
+      date: 'Yesterday',
+      id: 5,
+      read: false
+    },
+    {
+      fromName: 'Moe Chamont',
+      subject: 'Family Calendar - Version 1',
+      date: 'Last Week',
+      id: 6,
+      read: false
+    },
+    {
+      fromName: 'Kelly Richardson',
+      subject: 'Placeholder Headhots',
+      date: 'Last Week',
+      id: 7,
+      read: false
+    }
+  ];
+
+  constructor() { }
+
+  public getMessages(): Message[] {
+    return this.messages;
+  }
+
+  public getMessageById(id: number): Message {
+    return this.messages[id];
+  }
+}
